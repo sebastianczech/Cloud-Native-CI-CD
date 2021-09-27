f3dbc2ccf1fbc1a5ff194cea46dbc7afe3ff54b7
Sebastian Czech <sebaczech@gmail.com>
2021-09-27 23:54:24 +0200

    Cloud native CD

---
+++ design/cloud_native_cd.py
@@ -0,0 +1,50 @@
+# diagram.py
+from diagrams import Diagram, Cluster, Edge
+
+from diagrams.programming.language import Python
+from diagrams.onprem.vcs import Git, Github
+from diagrams.onprem.container import Docker
+from diagrams.onprem.iac import Terraform
+from diagrams.onprem.cd import Tekton
+
+with Diagram("Cloud native CD", show=False):
+
+    with Cluster("Continuous deployment pipeline - production environment"):
+        app = Git("app")
+        infra_prod = Terraform("prod")
+        pytest = Python("pytest")
+        registry = Docker("registry")
+        cd = Tekton("cd")
+
+        app >> Edge(label="pull request on branch master to build image", style="dashed") >> registry 
+        registry >> Edge(label="update image", style="dashed") >> cd 
+        cd >> Edge(label="provision prod infra") >> infra_prod
+        infra_prod >> Edge(label="integration tests") >> pytest 
+        pytest >> Edge(label="build image prod tag") >> registry
+
+    with Cluster("Continuous deployment pipeline - staging environment"):
+        app = Git("app")
+        infra_stage = Terraform("stage")
+        pytest = Python("pytest")
+        registry = Docker("registry")
+        cd = Tekton("cd")
+
+        app >> Edge(label="pull request on branch stage to build image", style="dashed") >> registry 
+        registry >> Edge(label="update image", style="dashed") >> cd 
+        cd >> Edge(label="provision stage infra") >> infra_stage
+        infra_stage >> Edge(label="integration tests") >> pytest 
+        pytest >> Edge(label="build image prod tag") >> registry
+
+    with Cluster("Continuous deployment pipeline - development environment"):
+        app = Git("app")
+        infra_dev = Terraform("dev")
+        pytest = Python("pytest")
+        registry = Docker("registry")
+        cd = Tekton("cd")
+
+        app >> Edge(label="push commit on every branch to build image", style="dashed") >> registry 
+        registry >> Edge(label="update image", style="dashed") >> cd 
+        cd >> Edge(label="provision dev infra") >> infra_dev
+        infra_dev >> Edge(label="integration tests") >> pytest 
+        pytest >> Edge(label="build image stage tag") >> registry
+        
