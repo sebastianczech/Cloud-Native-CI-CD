a5c2e3c17b8f45449bbbf0f7dbca7766c4e92afe
Sebastian Czech <sebaczech@gmail.com>
2021-09-28 23:51:33 +0200

    List additional settings

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 # import boto3
 import localstack_client.session as boto3
 
+import socket
+import os
+import datetime
+
+# Print host, config and time
+print("HOST: " + socket.gethostname())
+print("CONFIG: " + str(os.environ.get('PYTHON_HOSTNAME_ENV_VERSION')))
+print("TIME: ", datetime.datetime.now().strftime("%H:%M:%S"))
+
 # Let's use Amazon S3
 s3 = boto3.resource('s3')
 
 # Print out bucket names
-print("S3 buckets:")
+print("\nS3 buckets:")
 for bucket in s3.buckets.all():
     print("- " + bucket.name)
