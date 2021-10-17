13e74964e1eb01a94123f375b2230da017ffd133
Sebastian Czech <sebaczech@gmail.com>
2021-10-17 09:39:15 +0200

    Show localstack services

--- app/src/main.py
+++ app/src/main.py
@@ -18,6 +18,12 @@ def info_time():
     return datetime.datetime.now().strftime("%H:%M:%S")
 
 
+def localstack_services():
+    print("\nLOCALSTACK SERVICES:")
+    for service in boto3.config.get_service_endpoints():
+        print(service + " ---> " + boto3.config.get_service_endpoints()[service])
+
+
 def s3_list_buckets():
     s3 = boto3.resource('s3')
     return s3.buckets.all()
@@ -28,6 +34,9 @@ if __name__ == "__main__":
     print("HOST: " + info_hostname())
     print("CONFIG: " + info_config())
     print("TIME: ", info_time())
+
+    # Print Localstack services
+    localstack_services()
 
     # Print out bucket names
     print("\nS3 buckets:")
