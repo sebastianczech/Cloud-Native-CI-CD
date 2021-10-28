b7668b76eaa5cd9f5cf80437a88fe7b80c9ec99d
Sebastian Czech <sebaczech@gmail.com>
2021-10-28 23:48:17 +0200

    Test to check if bucket can be created

--- infra/localstack/test_localstack.py
+++ infra/localstack/test_localstack.py
@@ -1,6 +1,6 @@
 import requests
 import os
-
+import boto3
 
 def localstack_url(path):
     protocol = os.environ.get('INFRA_LOCALSTACK_PROTOCOL') if os.environ.get('INFRA_LOCALSTACK_PROTOCOL') is not None else 'http'
@@ -29,3 +29,24 @@ def test_localstack_s3_service_is_running():
     # then
     assert response.status_code == 200
     assert response.json()['services']['s3'] == 'running'
+
+
+def test_localstack_bucket_can_be_created():
+    # given
+    s3 = boto3.client('s3',
+                      endpoint_url=localstack_url(''),
+                      use_ssl=False,
+                      aws_access_key_id='test',
+                      aws_secret_access_key='test',
+                      region_name='us-east-1')
+    bucket_name = 'demo-bucket-py-test'
+
+    # when
+    response_create_bucket = s3.create_bucket(Bucket=bucket_name)
+    response_list_buckets = s3.list_buckets()
+    buckets_names = [bucket['Name'] for bucket in response_list_buckets['Buckets']]
+
+    # then
+    assert response_create_bucket['ResponseMetadata']['HTTPStatusCode'] == 200
+    assert response_list_buckets['ResponseMetadata']['HTTPStatusCode'] == 200
+    assert bucket_name in buckets_names
