fa898928a7f991370e95fd51a9426866dd6ca6bb
Sebastian Czech <sebaczech@gmail.com>
2022-01-04 22:50:47 +0100

    List SNS topics

--- app/src/main_boto3.py
+++ app/src/main_boto3.py
@@ -50,6 +50,10 @@ def s3_list_object_in_bucket(s3, bucket):
     return s3.list_objects(Bucket=bucket)['Contents']
 
 
+def sns_list_topics(sns):
+    return sns.list_topics()
+
+
 def localstack_config():
     return LocalstackConfig(endpoint_url=localstack_url(),
                             use_ssl=False,
@@ -58,13 +62,13 @@ def localstack_config():
                             region_name='us-east-1')
 
 
-def localstack_config_s3(localstackConfig):
-    return boto3.client('s3',
-                        endpoint_url=localstackConfig.endpoint_url,
-                        use_ssl=localstackConfig.use_ssl,
-                        aws_access_key_id=localstackConfig.aws_access_key_id,
-                        aws_secret_access_key=localstackConfig.aws_secret_access_key,
-                        region_name=localstackConfig.region_name)
+def localstack_config_for_service(service, config):
+    return boto3.client(service,
+                        endpoint_url=config.endpoint_url,
+                        use_ssl=config.use_ssl,
+                        aws_access_key_id=config.aws_access_key_id,
+                        aws_secret_access_key=config.aws_secret_access_key,
+                        region_name=config.region_name)
 
 
 if __name__ == "__main__":
@@ -75,17 +79,22 @@ if __name__ == "__main__":
     print("TIME: ", info_time())
 
     # Create bucket if not exists
-    s3_create_bucket(localstack_config_s3(localstack_config()), 'demo-bucket-py')
+    s3_create_bucket(localstack_config_for_service('s3', localstack_config()), 'demo-bucket-py')
 
     # Upload binary data
-    s3_upload_file(localstack_config_s3(localstack_config()), 'demo-bucket-py')
+    s3_upload_file(localstack_config_for_service('s3', localstack_config()), 'demo-bucket-py')
 
     # Print out bucket names
     print("\nS3 buckets:")
-    for bucket in s3_list_buckets(localstack_config_s3(localstack_config()))['Buckets']:
+    for bucket in s3_list_buckets(localstack_config_for_service('s3', localstack_config()))['Buckets']:
         print("- " + bucket['Name'])
 
     # Print out bucket files
     print("\nFiles in demo-bucket-py:")
-    for obj in s3_list_object_in_bucket(localstack_config_s3(localstack_config()), 'demo-bucket-py'):
+    for obj in s3_list_object_in_bucket(localstack_config_for_service('s3', localstack_config()), 'demo-bucket-py'):
         print("- " + obj['Key'])
+
+    # Print out topics names
+    print("\nSNS topic:")
+    for topic in sns_list_topics(localstack_config_for_service('sns', localstack_config()))['Topics']:
+        print("- " + topic['TopicArn'])
